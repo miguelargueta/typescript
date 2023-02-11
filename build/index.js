ec29db02f8cfcc32c3f3910a5ac849f808643d61
miguelargueta <miguel.argueta@unam.mx>
2023-02-11 13:02:18 -0600

    tipado estático

--- build/index.js
+++ build/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('hola');
+let text = 'hola';
+text = 'Hola2';
+console.log(text);
+function suma(num1, num2) {
+    return num1 + num2;
+}
+console.log(suma(5, 5));
+let booleanos = false;
+let a = '';
+let arreglos = ['hola', a, text];
+let objetos = { id: 1, nombre: 'Pedor' };
+let nulos = null;
+let cualquierDato = ''; //no se recomienda
+// null no es lo mismo que void, null es considerado un valor
+//mientras que void es ausencia de valor
+function imprimirEnConsola(mensaje) {
+    console.log(mensaje);
+}
+//la palabra undefines para saber si una propiedad esta definida 
+//let b : object = {id: 2}
+//if (b.nombre === undefined){
+//    b = {id: b.id, nombre:''}
+//}
--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log('hola')
+let text: string = 'hola'
+text = 'Hola2'
+
+console.log( text)
+
+function suma(num1: number, num2: number): number {
+    return num1 + num2
+
+}
+
+console.log(suma(5, 5))
+
+let booleanos : boolean = false
+let a: string =''
+let arreglos: string[] = ['hola', a, text]
+let objetos: object ={id: 1, nombre: 'Pedor'}
+let nulos: null = null
+let cualquierDato: any = '' //no se recomienda
+
+// null no es lo mismo que void, null es considerado un valor
+//mientras que void es ausencia de valor
+function imprimirEnConsola ( mensaje: string): void {
+    console.log (mensaje)
+}
+
+//la palabra undefines para saber si una propiedad esta definida 
+//let b : object = {id: 2}
+
+//if (b.nombre === undefined){
+//    b = {id: b.id, nombre:''}
+//}
+
