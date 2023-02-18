0e6a5e0b477b026850bbef91415ccdbf6ce5d610
miguelargueta <miguel.argueta@unam.mx>
2023-02-18 11:20:28 -0600

    variables let

---
+++ build/src/sesion-2/let.js
@@ -0,0 +1,13 @@
+"use strict";
+var index = 0;
+if (index === 0) {
+    var index = 2;
+    console.log(`index = ${index}`);
+}
+console.log(`index = ${index}`);
+let indexLet = 0;
+if (indexLet === 0) {
+    let indexLet = 2;
+    console.log(`indexLet = ${indexLet}`);
+}
+console.log(`indexLet = ${indexLet}`);
---
+++ src/sesion-2/let.ts
@@ -0,0 +1,16 @@
+var index:number =0
+if( index ===0) {
+    var index = 2
+    console.log(`index = ${index}`)
+}
+ 
+console.log(`index = ${index}`)
+
+
+let indexLet:number =0
+if( indexLet === 0) {
+    let indexLet = 2
+    console.log(`indexLet = ${indexLet}`)
+}
+ 
+console.log(`indexLet = ${indexLet}`)
