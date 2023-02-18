b271129a98a1c0deff321a57e94993eb8eb0b323
miguelargueta <miguel.argueta@unam.mx>
2023-02-18 13:14:13 -0600

    expresiones

---
+++ build/src/sesion-2/expresiones.js
@@ -0,0 +1,15 @@
+"use strict";
+const value = 10;
+/*let message: string = (value > 10) ? 'verdadero' : 'falso'
+
+if ( value > 9) {
+message = 'verdadero'
+} else {
+    message = 'false'
+}
+console.log (message)
+*/
+const message = value > 10 ?
+    'El valor es mayor que 10' :
+    'El valor es menor o igual a 10';
+console.log(message);
