30d4c9040d7147f26aba42ea08644265d3373b9e
miguelargueta <miguel.argueta@unam.mx>
2023-02-25 09:59:38 -0600

    operador coalescente nulo

--- build/src/sesion-2/expresiones.js
+++ build/src/sesion-2/expresiones.js
@@ -13,3 +13,9 @@ const message = value > 10 ?
     'El valor es mayor que 10' :
     'El valor es menor o igual a 10';
 console.log(message);
+function nullishCheck(a) {
+    console.log('a =' + (a !== null && a !== void 0 ? a : ' es nulo o indefinido'));
+}
+nullishCheck(1);
+nullishCheck(null);
+nullishCheck(undefined);
--- src/sesion-2/expresiones.ts
+++ src/sesion-2/expresiones.ts
@@ -14,4 +14,11 @@ const message: string = value > 10 ?
 'El valor es menor o igual a 10'
 console.log(message)
 
+function nullishCheck (a: number | null | undefined) {
+    console.log('a =' + (a ?? ' es nulo o indefinido'))
 
+}
+
+nullishCheck(1)
+nullishCheck( null)
+nullishCheck(undefined)
