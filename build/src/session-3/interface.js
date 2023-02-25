50fb5fbc9bc22cef6ad08d9acb93a6c072ddea45
miguelargueta <miguel.argueta@unam.mx>
2023-02-25 13:16:19 -0600

    Interfaces

---
+++ build/src/session-3/interface.js
@@ -0,0 +1,5 @@
+"use strict";
+let idName = {
+    id: 1,
+    name: 'Miguel'
+};
---
+++ src/session-3/interface.ts
@@ -0,0 +1,14 @@
+interface IdNace {
+    id: number;
+    name: string;
+}
+let idName:IdName ={
+    id: 1,
+    name: 'Miguel'
+}
+interface IdName {
+    id?: number;
+    name?: string;
+   
+}
+
