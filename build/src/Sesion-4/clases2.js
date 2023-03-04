db7d5d2dc219385a0e622c858935f5589206a28c
miguelargueta <miguel.argueta@unam.mx>
2023-03-04 12:02:43 -0600

    clases - private

--- build/src/Sesion-4/clases2.js
+++ build/src/Sesion-4/clases2.js
@@ -6,3 +6,22 @@ class ClassWhitConstructor {
 }
 let classWC = new ClassWhitConstructor(10);
 console.log(`classwc = ${JSON.stringify(classWC)}`);
+class ClassWithPublicProperty {
+}
+let MyClassWithPublicP = new ClassWithPublicProperty();
+MyClassWithPublicP.id = 10;
+console.log(MyClassWithPublicP.id);
+class ClassWithPrivateProperty {
+    constructor(id) {
+        this.id = id;
+    }
+    print() {
+        console.log(this.id);
+    }
+}
+let myClassWithPrivateP = new ClassWithPrivateProperty(202);
+// No podemos acceder a una propiedad privada
+//fuera de la clase por lo que el siguiente renglon no sirve, marca error
+//myClassWithPrivateP.id = 20
+// pero si utilizar metodos públicos para acceder a ellos, como con el print.
+myClassWithPrivateP.print();
--- src/Sesion-4/clases2.ts
+++ src/Sesion-4/clases2.ts
@@ -6,3 +6,29 @@ class ClassWhitConstructor {
 }
 let classWC = new ClassWhitConstructor(10)
 console.log (`classwc = ${JSON.stringify(classWC)}`)
+
+class ClassWithPublicProperty {
+    id: number | undefined
+}
+
+let MyClassWithPublicP = new ClassWithPublicProperty () 
+
+MyClassWithPublicP.id =10
+console.log(MyClassWithPublicP.id)
+
+class ClassWithPrivateProperty {
+    private id: number
+    constructor (id: number) {
+        this.id = id
+    }
+    print() {
+        console.log(this.id)
+    }
+}
+
+let myClassWithPrivateP = new ClassWithPrivateProperty(202)
+// No podemos acceder a una propiedad privada
+//fuera de la clase por lo que el siguiente renglon no sirve, marca error
+//myClassWithPrivateP.id = 20
+// pero si utilizar metodos públicos para acceder a ellos, como con el print.
+myClassWithPrivateP.print()
