d69f3fcfc3404e69151f14570415d51f266e5f47
miguelargueta <miguel.argueta@unam.mx>
2023-03-04 10:50:22 -0600

    clases implementando  interfases

--- build/src/Sesion-4/clases.js
+++ build/src/Sesion-4/clases.js
@@ -10,3 +10,29 @@ mySimpleClass.print();
 let mySimpleClassA = new SimpleClass();
 mySimpleClassA.id = 30;
 mySimpleClassA.print();
+//implementación de interfaces
+class ClassA {
+    print() {
+        console.log('ClassA.print() HA sido llamado');
+    }
+}
+class ClassB {
+    print() {
+        console.log('ClassB.print() HA sido llamado');
+    }
+}
+function printClass(a) {
+    a.print();
+}
+let myClassA = new ClassA();
+let myClassB = new ClassB();
+printClass(myClassA);
+printClass(myClassB);
+//tipado pato
+class ClassC {
+    print() {
+        console.log('ClassC.print() HA sido llamado C');
+    }
+}
+let myClassC = new ClassC();
+printClass(myClassC);
--- src/Sesion-4/clases.ts
+++ src/Sesion-4/clases.ts
@@ -12,3 +12,42 @@ mySimpleClass.print()
 let mySimpleClassA = new SimpleClass()
 mySimpleClassA.id = 30
 mySimpleClassA.print()
+
+//implementación de interfaces
+class ClassA  implements Print{
+    print(){
+        console.log ('ClassA.print() HA sido llamado')
+    }
+}
+
+class ClassB implements Print {
+        print(){
+            console.log ('ClassB.print() HA sido llamado')
+        }
+}
+interface Print {
+        print (): void;
+    }
+
+function printClass (a: Print){
+        a.print()
+    }
+
+let myClassA = new ClassA()
+let myClassB = new ClassB()
+
+printClass(myClassA)
+printClass(myClassB)
+
+
+//tipado pato
+class ClassC {
+    print(){
+        console.log ('ClassC.print() HA sido llamado C')
+    }
+}
+let myClassC = new ClassC()
+printClass(myClassC)
+
+
+    
