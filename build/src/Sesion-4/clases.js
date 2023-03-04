1f70c7ecfd7d8bb6bcf4a968fb049824ac8bc095
miguelargueta <miguel.argueta@unam.mx>
2023-03-04 10:36:03 -0600

    palabra reservada This

--- build/src/Sesion-4/clases.js
+++ build/src/Sesion-4/clases.js
@@ -1,8 +1,12 @@
 "use strict";
 class SimpleClass {
     print() {
-        console.log('SimpleClass.print() ha sido llamado ');
+        console.log(`SimpleClass.id = ${this.id} `);
     }
 }
 let mySimpleClass = new SimpleClass();
+mySimpleClass.id = 10;
 mySimpleClass.print();
+let mySimpleClassA = new SimpleClass();
+mySimpleClassA.id = 30;
+mySimpleClassA.print();
--- src/Sesion-4/clases.ts
+++ src/Sesion-4/clases.ts
@@ -1,12 +1,14 @@
 class SimpleClass {
     id: number | undefined;
     print(): void {
-        console.log('SimpleClass.print() ha sido llamado ')
+        console.log(`SimpleClass.id = ${this.id} ` )
     }
-
 }
 
 let mySimpleClass = new SimpleClass()
-
-
+mySimpleClass.id = 10
 mySimpleClass.print()
+
+let mySimpleClassA = new SimpleClass()
+mySimpleClassA.id = 30
+mySimpleClassA.print()
