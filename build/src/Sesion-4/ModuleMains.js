6aa24bac349945cb549d870581b0df98a23a78f6
miguelargueta <miguel.argueta@unam.mx>
2023-03-18 10:08:17 -0600

    Default exports

--- build/src/Sesion-4/ModuleMains.js
+++ build/src/Sesion-4/ModuleMains.js
@@ -1,10 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Module1_1 = require("./src/modules/Module1");
 // cada clase o método que exportemos 
 // tiene que ser separado por una coma
 const MultipleExports_1 = require("./src/modules/MultipleExports");
+const DefaultExport_1 = __importDefault(require("./src/modules/DefaultExport"));
 let myModule1 = new Module1_1.Module1();
 myModule1.print();
 let myMC1 = new MultipleExports_1.MultpleClass1();
 let myMC2 = new MultipleExports_1.MultpleClass2();
+let modDefault = (0, DefaultExport_1.default)(1, 2);
+console.log(modDefault);
--- src/Sesion-4/ModuleMains.ts
+++ src/Sesion-4/ModuleMains.ts
@@ -2,9 +2,13 @@ import { Module1 as MyMod1 } from "./src/modules/Module1";
 // cada clase o método que exportemos 
 // tiene que ser separado por una coma
 import { MultpleClass1, MultpleClass2 } from "./src/modules/MultipleExports";
+import Add, { ModuleNonDefaultExport } from "./src/modules/DefaultExport";
 
 let myModule1 = new MyMod1()
 myModule1.print()
 
 let myMC1 = new MultpleClass1()
 let myMC2 = new MultpleClass2()
+
+let modDefault = Add(1, 2)
+console.log(modDefault)
